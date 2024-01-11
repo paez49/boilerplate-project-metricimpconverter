6fe4aa46071fa9694ef33646ae891045e77da237
Juan Páez <jdpluc302@gmail.com>
2024-01-11 13:50:57 -0500

    get number and unit to convert

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,54 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+  const units = ["gal","lbs","mi","L","kg","km"]
+  this.getNum = function (input) {
+    const indexResult = input.search(/[a-zA-Z]/);
+
+    if (indexResult !== -1) {
+      return input.slice(0, indexResult);
+    } else {
+      return input;
+    }
   };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
+
+  this.getUnit = function (input) {
+    const result = input.match(/[a-zA-Z]+/);
+
+    if (result !== null) {
+      console.log(result[0])
+      if (units.includes(result[0].toLowerCase())){
+        console.log(result[0])
+        return result[0];
+      }
+    }
+    return null;
   };
-  
-  this.getReturnUnit = function(initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
     let result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.spellOutUnit = function (unit) {
     let result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,15 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
-
+  app.get('/api/convert', function (req, res) {
+    let initNum = convertHandler.getNum(req.query.input)
+    let initUnit = convertHandler.getUnit(req.query.input)
+    if (initNum == ""){
+      initNum = 1
+    }
+    if( initUnit == null){
+      res.type("txt").send("invalid unit");
+    }
+    res.type("txt").send(initUnit)
+  })
 };
